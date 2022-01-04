1c0f9fcdde0b5b4a7221dd2fe38f190417621e46
sarthakpython <96922090+sarthakpython@users.noreply.github.com>
2022-01-04 21:14:03 +0530

    Add files via upload

---
+++ checkfactors.py
@@ -0,0 +1,47 @@
+
+x = input("Write A Number")
+if x=="1":
+    print("1 COMES IN  TABLE OF 1")
+if x=="2":
+    print(("2 COMES IN TABLE OF 1 AND 2 "))
+if x=="3":
+    print("3 COMES IN TABLE OF 1, AND 3")
+if x=="4":
+    print("4 COMES IN TABLE OF 1,2 AND 4")
+if x=="5":
+    print("5 COMES IN TABLE OF 1 AND 5 ")
+if x=="6":
+    print("6 COMES IN TABLE OF 1,2 AND 3")
+if x=="7":
+    print("7 COMES IN TABLE OF 1 AND 7")
+if x=="8":
+    print("8 COMES IN TABLE OF 1,2,4 AND 8")
+if x=="9":
+    print("9 COMES IN TABLE OF 1,3 AND 9")
+if x=="10":
+    print("10 COMES IN TABLE OF 1,2,5 AND 10")
+if x=="11":
+    print("11 COMES IN TABLE OF 1 AND 11")
+if x=="12":
+    print("12 COMES IN TABLE OF 1,2,4,6 AND 12")
+if x=="13":
+    print("13 COMES IN TABLE OF 1 AND 13")
+if x=="14":
+    print("14 COMES IN TABLE OF 1,2,7 AND 14")
+if x=="15":
+    print("15 COMES IN TABLE OF 1,3,5 AND 15")
+if x=="16":
+    print("16 COMES IN TABLE OF 1,2,4,8 AND 16")
+if x=="17":
+    print("17 COMES IN TABLE OF 1 AND 17")
+if x=="18":
+    print("18 COMES IN TABLES OF 1,2,3,6,9 AND 18")
+if x=="19":
+    print("19 COMES IN TABLES OF 1 AND 19")
+if x=="20":
+    print("20 COMES IN TABLES PF 1,2,4,5,10 AND 20")
+print("Made By Sarthak")
+print("Curently I am Working On More Numbers")
+    #print("Sorry! Currently We Are Working On 20 to 100 Numbers")
+#if x>100:
+    #print("Its Not Possible To Provide Numbers More Than 100")
